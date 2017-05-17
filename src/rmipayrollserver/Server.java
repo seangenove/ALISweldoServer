8726904036be38352d01dcd2d8492295db3c9235
cynlai <cynlamercedes@gmail.com>
2017-05-17 23:07:08 +0800

    Added computeSSS method

--- src/rmipayrollserver/Server.java
+++ src/rmipayrollserver/Server.java
@@ -103,33 +103,96 @@ public class Server extends UnicastRemoteObject implements AddEmployee {
         if(computedGrossIncome <= 1249.99){
             computedGrossIncome -= 36.30;
             SSS = 36.30;
+        } else if(computedGrossIncome <= 1749.99){
+            computedGrossIncome -= 54.50;
+            SSS = 54.50;
+        } else if(computedGrossIncome <= 2249.99){
+            computedGrossIncome -= 72.70;
+            SSS = 72.70;
+        } else if(computedGrossIncome <= 2749.99){
+            computedGrossIncome -= 90.80;
+            SSS = 90.80;
+        } else if(computedGrossIncome <= 3249.99){
+            computedGrossIncome -= 109.00;
+            SSS = 109.00;
+        } else if(computedGrossIncome <= 3749.99){
+            computedGrossIncome -= 127.20;
+            SSS = 127.20;
+        } else if(computedGrossIncome <= 4249.99){
+            computedGrossIncome -= 145.30;
+            SSS = 145.30;
+        } else if(computedGrossIncome <= 4749.99){
+            computedGrossIncome -= 163.50;
+            SSS = 163.50;
+        } else if(computedGrossIncome <= 5249.99){
+            computedGrossIncome -= 181.70;
+            SSS = 181.70;
+        } else if (computedGrossIncome <= 5749.99){
+            computedGrossIncome -= 199.80;
+            SSS = 199.80;
+        } else if (computedGrossIncome <= 6249.99){
+            computedGrossIncome -= 218.00;
+            SSS = 218.00;
+        } else if (computedGrossIncome <= 6749.99){
+            computedGrossIncome -= 236.20;
+            SSS = 236.20;
+        } else if (computedGrossIncome <= 7249.99){
+            computedGrossIncome -= 254.30;
+            SSS = 254.30;
+        } else if (computedGrossIncome <= 7749.99){
+            computedGrossIncome -= 272.50;
+            SSS = 272.50;
+        } else if(computedGrossIncome <= 8249.99){
+            computedGrossIncome -= 290.70;
+            SSS = 290.70;
+        } else if(computedGrossIncome <= 8749.99){
+            computedGrossIncome -= 308.80;
+            SSS = 308.80;
+        } else if(computedGrossIncome <= 9249.99){
+            computedGrossIncome -= 327.00;
+            SSS = 327.00;
+        } else if(computedGrossIncome <= 9749.99){
+            computedGrossIncome -= 345.20;
+            SSS = 90.80;
+        } else if(computedGrossIncome <= 10249.99){
+            computedGrossIncome -= 363.30;
+            SSS = 363.30;
+        } else if (computedGrossIncome <= 10749.99){
+            computedGrossIncome -= 381.50;
+            SSS = 381.50;
+        } else if(computedGrossIncome <= 12249.99){
+            computedGrossIncome -= 436.00;
+            SSS = 436.00;
+        } else if(computedGrossIncome <= 12749.99){
+            computedGrossIncome -= 454.20;
+            SSS = 454.20;
+        } else if(computedGrossIncome <= 13249.99){
+            computedGrossIncome -= 472.30;
+            SSS = 472.30;
+        } else if(computedGrossIncome <= 13749.99){
+            computedGrossIncome -= 490.50;
+            SSS = 490.50;
+        } else if(computedGrossIncome <= 14249.99){
+            computedGrossIncome -= 508.70;
+            SSS = 508.70;
+        } else if(computedGrossIncome <= 14749.99){
+            computedGrossIncome -= 526.80;
+            SSS = 526.80;
+        } else if(computedGrossIncome <= 15249.99){
+            computedGrossIncome -= 545.00;
+            SSS = 545.00;
+        } else {
+            computedGrossIncome -= 563.20;
+            SSS = 563.20;
+        } 
+        
+        if (operation.equals("get")) {
+            return SSS;
+        } else {
+            return computedGrossIncome;
         }
         
-        return 20.00;
-    }
-//else if(){
-//        }else if(){
-//        }else if(){
-//        }else if(){
-//        }else if(){
-//        }else if(){
-//        }else if(){
-//        }else if(){
-//        }else if(){
-//        }else if(){
-//        }else if(){
-//        }else if(){
-//        }else if(){
-//        }else if(){
-//        }else if(){
-//        }else if(){
-//        }else if(){
-//        }else if(){
-//        }else if(){
-//        }else if(){
-//        }
         
-        return 20.00;
     }
 
     public static void main(String[] args) {
